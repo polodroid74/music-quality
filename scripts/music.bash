8a3e99337c2951524a417b962a24c18e7753bf34
Paul <polobouton@gmail.com>
2021-04-24 18:17:49 +0200

    REFACTOR : Split bash script

--- scripts/music.bash
+++ scripts/music.bash
@@ -1,35 +1,9 @@
 #!/bin/bash
 
-########################
-#   Useful Variables   #
-########################
-COLOR_DEFAULT="\e[39m"
-COLOR_GREEN="\e[92m"
-COLOR_RED="\e[31m"
-COLOR_MAGENTA="\e[95m"
-RESET="\e[0m"
 current_dir=$(pwd)
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-########################
-#   Useful Functions   #
-########################
-
-parse_formats (){
-local formats_list="$1"
-declare -i n=0
-for format in ${formats_list}; do
-	declare -i curr=$(find "${music_dir}" -name "*.${format}" | wc -l)
-	n=$n+$curr
-done
-echo $n
-}
-
-if [ "$#" -ne 1 ]; then
-	echo -e "${COLOR_RED}Wrong number of arguments !${RESET}"
-	echo -e "${COLOR_GREEN}USAGE : ${RESET} $0 ${COLOR_MAGENTA} dir_to_analyze${RESET}"
-	exit 0
-fi
+source "${script_dir}/sources/functions.bash"
 
 music_dir="$1"
 echo -e "Music dir set to : ${music_dir}${RESET}"
