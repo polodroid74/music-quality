5089ee50fa13cbe607c379c815da8828c6c899eb
Paul <polobouton@gmail.com>
2021-05-01 15:15:20 +0200

    FEAT : Add average bitrate information

--- scripts/music.bash
+++ scripts/music.bash
@@ -1,30 +1,42 @@
 #!/bin/bash
-
 current_dir=$(pwd)
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "${script_dir}/sources/functions.bash"
 
-music_dir="$1"
+if [ "$#" -ne 1 ]; then
+	echo -e "${COLOR_RED}Wrong number of arguments !${RESET}"
+	echo -e "${COLOR_GREEN}USAGE : ${RESET} $0 ${COLOR_MAGENTA} dir_to_analyze${RESET}"
+	exit 0
+fi
+
+music_dir=$(getAbsolutePath "$1")
 echo -e "Music dir set to : ${music_dir}${RESET}"
 
 lossless_formats="flac alac aiff wav"
 compressed_formats="mp3 aac m4a ogg"
+all_formats="${lossless_formats} ${compressed_formats}"
+
 
 echo -e "${COLOR_MAGENTA}Lossless formats : ${lossless_formats}${RESET}"
 echo -e "Parsing lossless formats."
-declare -i n_lossless=$(parse_formats "${lossless_formats}")
+declare -i n_lossless=$(parseFormats "${lossless_formats}" "${music_dir}")
 
 echo -e "${COLOR_MAGENTA}Compressed formats : ${compressed_formats}${RESET}"
 echo -e "Parsing compressed formats."
-declare -i n_compressed=$(parse_formats "${compressed_formats}")
+declare -i n_compressed=$(parseFormats "${compressed_formats}" "${music_dir}")
 
+echo -e "Computing Quality Ratio"
 declare -i numerator=$((10000*$n_lossless))
 declare -i quality_ratio=$(($numerator / ($n_lossless+$n_compressed)))
 declare -i int_part=$(($quality_ratio / 100))
 declare -i float_part=$(($quality_ratio - $int_part * 100))
 
+echo -e "Computing Average Bit Rate"
+declare -i avg_sample_rate=$(averageData "Bit rate" "${all_formats}" "${music_dir}")
+
 #Summary
-echo -e "${COLOR_RED}Found $n_lossless lossless files."
-echo -e "${COLOR_RED}Found $n_compressed compresssed files."
-echo -e "${COLOR_RED}Quality ratio = $int_part.$float_part%"
+echo -e "${COLOR_RED}Found $n_lossless lossless files.${RESET}"
+echo -e "${COLOR_RED}Found $n_compressed compresssed files.${RESET}"
+echo -e "${COLOR_RED}Quality ratio = $int_part.$float_part%${RESET}"
+echo -e "${COLOR_RED}Average Sample Rate = ${avg_sample_rate}kbps${RESET}"
--- scripts/sources/functions.bash
+++ scripts/sources/functions.bash
@@ -8,25 +8,59 @@ COLOR_GREEN="\e[92m"
 COLOR_RED="\e[31m"
 COLOR_MAGENTA="\e[95m"
 RESET="\e[0m"
-current_dir=$(pwd)
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ########################
 #   Useful Functions   #
 ########################
 
-parse_formats (){
+getAbsolutePath(){
+local cur_dir=$(pwd)
+local dir="$1"
+cd "${dir}"
+echo $(pwd)
+cd "${cur_dir}"
+}
+
+buildFindPatterns(){
 local formats_list="$1"
-declare -i n=0
+local find_pattern="-name"
 for format in ${formats_list}; do
-	declare -i curr=$(find "${music_dir}" -name "*.${format}" | wc -l)
-	n=$n+$curr
+	find_pattern="${find_pattern} '*${format}' -o -name"
 done
+local find_pattern=${find_pattern::-9}
+echo "${find_pattern}"
+}
+
+
+parseFormats(){
+local formats_list="$1"
+local dir=$(getAbsolutePath "$2")
+local formats_patterns=$(buildFindPatterns "${formats_list}")
+find_cmd="find "${dir}" -type f ${formats_patterns}"
+declare -i n=$(eval ${find_cmd} | wc -l)
 echo $n
 }
 
-if [ "$#" -ne 1 ]; then
-	echo -e "${COLOR_RED}Wrong number of arguments !${RESET}"
-	echo -e "${COLOR_GREEN}USAGE : ${RESET} $0 ${COLOR_MAGENTA} dir_to_analyze${RESET}"
-	exit 0
-fi
+averageData(){
+local data_field="$1"
+local formats_list="$2"
+local dir=$(getAbsolutePath "$3")
+
+declare -i data=0
+declare -i n=0
+local formats_patterns=$(buildFindPatterns "${formats_list}")
+
+find_cmd="find "${dir}" -type f ${formats_patterns}"
+local files=$(eval ${find_cmd})
+
+while read file; do
+	declare -i file_data=$(mediainfo "${file}" | grep "${data_field}[ ]*:" | tr -dc '0-9')
+	if [ "$file_data" -ne 0 ]; then
+		data=$data+$file_data
+		n=$n+1
+	fi
+done <<< $(eval ${find_cmd})
+
+local data=$((${data} / $n))
+echo ${data}
+}
