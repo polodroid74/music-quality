0e5fb66eaaf11a1270554fce8de658b7f842187f
Paul <polobouton@gmail.com>
2021-05-07 21:05:13 +0200

    FEAT : Add the possibility to only compute the quality ratio

--- scripts/music.bash
+++ scripts/music.bash
@@ -5,13 +5,14 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${script_dir}/sources/functions.bash"
 
 echo -e "${COLOR_GREEN}STARTING ANALYSIS${RESET}"
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
 	echo -e "${COLOR_RED}Wrong number of arguments !${RESET}"
-	echo -e "${COLOR_GREEN}USAGE : ${RESET} $0 ${COLOR_MAGENTA} dir_to_analyze${RESET}"
+	echo -e "${COLOR_GREEN}USAGE : ${RESET} $0 ${COLOR_MAGENTA} dir_to_analyze only_sample_ratio(y/n)${RESET}"
 	exit 0
 fi
 
 music_dir=$(getAbsolutePath "$1")
+only_sr="${2}"
 echo -e "Music dir set to : ${music_dir}${RESET}"
 
 lossless_formats="flac alac aiff wav wma"
@@ -43,6 +44,17 @@ declare -i quality_ratio=$(($numerator / ($n_lossless+$n_compressed)))
 declare -i int_part=$(($quality_ratio / 100))
 declare -i float_part=$(($quality_ratio - $int_part * 100))
 echo -e "Quality ratio = $int_part.$float_part%${RESET}"
+
+if [ "${only_sr}" == "y" ]; then
+	#Summary
+	echo -e ""
+	echo -e "${COLOR_GREEN}ANALYSIS SUMMARY${RESET}"
+	echo -e "${COLOR_RED}${n_files} Music files${RESET}"
+	echo -e "${COLOR_RED}Found $n_lossless lossless files.${RESET}"
+	echo -e "${COLOR_RED}Found $n_compressed compresssed files.${RESET}"
+	echo -e "${COLOR_RED}Quality ratio = $int_part.$float_part%${RESET}"
+	exit 0
+fi
 
 echo -e "Computing Average Bitrate"
 declare -i avg_bitrate=$(averageData "BitRate" "${all_formats}" "${music_dir}")
